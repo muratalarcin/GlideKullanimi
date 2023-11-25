e2a50ff158a9df16f0e50be022bcf4d1bbf0b597
Murat Alarcin <murattrbl@icloud.com>
2023-11-25 19:29:57 +0300

    glide

--- app/src/main/java/com/muratalarcin/glidekullanimi/MainActivity.java
+++ app/src/main/java/com/muratalarcin/glidekullanimi/MainActivity.java
@@ -4,11 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
+import com.muratalarcin.glidekullanimi.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        resimGoster("baklava.png");
+
+        binding.buttonResim1.setOnClickListener(view -> {
+            resimGoster("kofte.png");
+        });
+
+        binding.buttonResim2.setOnClickListener(view -> {
+            resimGoster("ayran.png");
+        });
+
     }
+
+    public void resimGoster(String resimAdi) {
+        String url = "http://kasimadalan.pe.hu/yemekler/resimler/" + resimAdi;
+        Glide.with(this).load(url).override(300, 300).into(binding.imageView);
+    }
+
 }
